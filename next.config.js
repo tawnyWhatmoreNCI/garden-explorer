ee3d0d8db752870724ef6bfb9a418af570e4bc4a
tawnyWhatmoreNCI <157423950+tawnyWhatmoreNCI@users.noreply.github.com>
2024-08-13 14:40:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  output: 'export',
+  output: 'standalone',
   webpack(config) {
     // Grab the existing rule that handles SVG imports
     const fileLoaderRule = config.module.rules.find((rule) =>
